4ce5063a5cb50dd7e70aa75761fd802c52b7f71e
JohnDelcano <bhoxse.delcano@gmail.com>
2025-11-01 06:14:12 +0800

    G

--- api/students.js
+++ api/students.js
@@ -480,7 +480,7 @@ router.post("/register", async (req, res) => {
       email: emailLower,
       password: hash,
       profilePicture,
-      validIDs: [frontIdUrl, backIdUrl],
+      validIDs,
       birthday: birthdayDate,
       phone: phone ? Number(phone) : undefined,
       address,
